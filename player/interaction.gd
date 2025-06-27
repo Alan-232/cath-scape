3246b9c54f8ad82a05b5123c66a1204de8513922
Alan-232 <alanpraju4@gmail.com>
2025-06-27 09:15:56 +0530

    door not working

--- player/interaction.gd
+++ player/interaction.gd
@@ -3,7 +3,7 @@ extends RayCast3D
 func _physics_process(delta: float) -> void:
 	if is_colliding():
 		var hit = get_collider()
-		if hit.name == "door":
-			if Input.is_action_just_pressed("interact"):
-				hit.get_parent().get_parent().get_parent().toggle_door()
-			
+		if hit.name == "hinge" and Input.is_action_just_pressed("interact"):
+			var door_node = hit.get_parent().get_parent()
+			if "toggle_door" in door_node:
+				door_node.toggle_door()
